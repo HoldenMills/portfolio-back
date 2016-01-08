f0c1d5b7121207efc04933370c5756fa611c2a9d
Holden Mills <mholdenmills@gmail.com>
2016-01-07 22:10:02 -0500

    Adds mail form
    Adds controller, model and routes.  Configs smpt settings using heroku

--- api/app/models/contact.rb
+++ api/app/models/contact.rb
@@ -7,7 +7,7 @@ class Contact < MailForm::Base
   def headers
     {
       :subject => "My Contact Form",
-      :to => "your_email@example.org",
+      :to => "mholdenmills@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
   end
